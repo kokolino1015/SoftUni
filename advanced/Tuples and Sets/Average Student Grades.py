47f5c05255d73dd276ecd2644997a3dcd2ccc88b
kokolino1015 <71516863+kokolino1015@users.noreply.github.com>
2021-01-27 21:07:00 +0200

    Update Average Student Grades.py

--- advanced/Tuples and Sets/Average Student Grades.py
+++ advanced/Tuples and Sets/Average Student Grades.py
@@ -1,22 +1,18 @@
-count = int(input())
+num = int(input())
 
-students = {}
+numbers_dict = {}
+for num in range(num):
+    command = input().split(" ")
+    if command[0] not in numbers_dict:
+        numbers_dict[command[0]] = []
+    numbers_dict[command[0]].append(float(command[1]))
 
-for _ in range(count):
-    line = input().split()
-    student = line[0]
-    grade = float(line[1])
-    if student not in students:
-        students[student] = [float(grade)]
-    else:
-        students[student].append(float(grade))
-
-for student, grade in students.items():
-    string_of_grade = ''
-    for g in grade:
-        if len(str(g)) == 3:
-            g = str(g) + "0"
-        string_of_grade += str(g) + " "
-    avg = sum(grade)/len(grade)
-
-    print(f"{student} -> {string_of_grade}(avg: {avg:.2f})")
+for key, value in numbers_dict.items():
+    string = ''
+    for i in value:
+        if len(str(i)) < 4:
+            string += str(i) + '0 '
+        else:
+            string += str(i) + ' '
+    avr = sum(value) / len(value)
+    print(f"{key} -> {string}(avg: {avr:.2f})")

31e89dde8328cccf408abc18e9706645c3659ce1
kokolino1015 <71516863+kokolino1015@users.noreply.github.com>
2021-01-27 20:26:08 +0200

    Create Count Same Values.py

---
+++ advanced/Tuples and Sets/Count Same Values.py
@@ -0,0 +1,9 @@
+numbers = tuple(map(float, input().split()))
+
+numbers_dict = {}
+for num in numbers:
+    if num not in numbers_dict:
+        numbers_dict[num] = 0
+    numbers_dict[num] += 1
+
+[print(f"{key} - {value} times") for key, value in numbers_dict.items()]

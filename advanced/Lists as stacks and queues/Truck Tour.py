1cd589af9644a094cdb4fad250e806a439d5ec5d
kokolino1015 <71516863+kokolino1015@users.noreply.github.com>
2021-05-22 21:33:04 +0300

    Update Truck Tour.py

--- advanced/Lists as stacks and queues/Truck Tour.py
+++ advanced/Lists as stacks and queues/Truck Tour.py
@@ -1,34 +1,24 @@
 from collections import deque
 n = int(input())
-petrol = deque([])
-dist = deque([])
-tank = 0
-current_index = 0
-next_station = 0
+queue = deque([])
+
+for _ in range(n):
+    i = input().split()
+    queue.append([int(i[0]), int(i[1])])
 
 for i in range(n):
-    add_petrol, add_distance = input().split()
-    petrol.append(add_petrol)
-    dist.append(add_distance)
-
-petrol.append(petrol[0])
-dist.append(dist[0])
-
-while True:
-    add_to_tank = int(petrol[next_station]) - int(dist[next_station])
-    if tank + add_to_tank < 0:
-        current_index += 1
-        next_station = 0
-        tank = 0
-        petrol.popleft()
-        petrol.append(petrol[0])
-        dist.popleft()
-        dist.append(dist[0])
-        continue
-    else:
-        tank += add_to_tank
-        next_station += 1
-        if next_station == n:
+    go_through_all = 0
+    fuel_tank = 0
+    for gas_pumps in queue:
+        fuel, distance = gas_pumps[0], gas_pumps[1]
+        fuel_tank += fuel
+        if fuel_tank < distance:
             break
+        fuel_tank -= distance
+        go_through_all += 1
 
-print(current_index)
+    if go_through_all == len(queue):
+        print(i)
+        break
+    queue.rotate(-1)
+    

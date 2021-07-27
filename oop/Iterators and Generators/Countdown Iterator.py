5ee35cf56b49eefcecd5950fe5d80b769a88d3f8
kokolino1015 <71516863+kokolino1015@users.noreply.github.com>
2021-07-27 14:47:43 +0300

    Create Countdown Iterator.py

---
+++ oop/Iterators and Generators/Countdown Iterator.py
@@ -0,0 +1,13 @@
+class countdown_iterator:
+    def __init__(self, count):
+        self.count = count
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count == -1:
+            raise StopIteration
+        current_num = self.count
+        self.count -= 1
+        return current_num

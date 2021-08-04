f68665dc17c5bf4dff3b2113c525e0c5a2381f81
kokolino1015 <71516863+kokolino1015@users.noreply.github.com>
2021-08-04 16:50:07 +0300

    Update Car Manager.py

--- oop/Testing/Car Manager.py
+++ oop/Testing/Car Manager.py
@@ -2,170 +2,53 @@ import unittest
 
 
 class TestCar(unittest.TestCase):
-    def __get_exception_from_init(self, make, model, fuel_consumption, fuel_capacity):
-        with self.assertRaises(Exception) as context:
-            Car(make, model, fuel_consumption, fuel_capacity)
-        return context.exception
+    def setUp(self):
+        self.car = Car(fuel_capacity=100, fuel_consumption=5.6, make='Test', model='TestModel')
 
-    def test_carInit_CorrectValuesInitialization(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        car = Car(make, model, fuel_consumption, fuel_capacity)
-        expected = [make, model, fuel_consumption, fuel_capacity, 0]
-        actual = [car.make, car.model, car.fuel_consumption, car.fuel_capacity, car.fuel_amount]
-        self.assertListEqual(expected, actual)
+    def test_init_car(self):
+        self.assertEqual('Test', self.car.make)
+        self.assertEqual('TestModel', self.car.model)
+        self.assertEqual(100, self.car.fuel_capacity)
+        self.assertEqual(5.6, self.car.fuel_consumption)
 
-    def test_carInit_WithNoneMake_RaiseException(self):
-        make = None
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        exception = self.__get_exception_from_init(*params)
-        self.assertIsNotNone(exception)
+    def test_if_wrong_values_raise_exception_model(self):
+        with self.assertRaises(Exception) as ex:
+            self.car.model = ''
+        self.assertEqual("Model cannot be null or empty!", str(ex.exception))
 
-    def test_carInit_WithEmptyStringMake_RaiseException(self):
-        make = ""
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        exception = self.__get_exception_from_init(*params)
-        self.assertIsNotNone(exception)
+    def test_if_wrong_values_raise_exception_make(self):
+        with self.assertRaises(Exception) as ex:
+            self.car.make = ''
+        self.assertEqual("Make cannot be null or empty!", str(ex.exception))
 
-    def test_carInit_WithNoneModel_RaiseException(self):
-        make = "Test make"
-        model = None
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        exception = self.__get_exception_from_init(*params)
-        self.assertIsNotNone(exception)
+    def test_if_wrong_values_raise_exception_fuel_consumption(self):
+        with self.assertRaises(Exception) as ex:
+            self.car.fuel_consumption = 0
+        self.assertEqual("Fuel consumption cannot be zero or negative!", str(ex.exception))
 
-    def test_carInit_WithEmptyStringModel_RaiseException(self):
-        make = "Test make"
-        model = ""
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        exception = self.__get_exception_from_init(*params)
-        self.assertIsNotNone(exception)
+    def test_if_wrong_values_raise_exception_fuel_capacity(self):
+        with self.assertRaises(Exception) as ex:
+            self.car.fuel_capacity = 0
+        self.assertEqual("Fuel capacity cannot be zero or negative!", str(ex.exception))
 
-    def test_carInit_WithNegativeFuelConsumption_RaiseException(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = -1
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        exception = self.__get_exception_from_init(*params)
-        self.assertIsNotNone(exception)
+    def test_refuel_exception_case(self):
+        with self.assertRaises(Exception) as ex:
+            self.car.refuel(0)
+        self.assertEqual("Fuel amount cannot be zero or negative!", str(ex.exception))
 
-    def test_carInit_WithZeroFuelConsumption_RaiseException(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 0
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        exception = self.__get_exception_from_init(*params)
-        self.assertIsNotNone(exception)
+    def test_refuel_correct_case_v1(self):
+        self.car.refuel(10)
+        self.assertEqual(self.car.fuel_amount, 10)
 
-    def test_carInit_WithNegativeFuelCapacity_RaiseException(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = -1
-        params = [make, model, fuel_consumption, fuel_capacity]
-        exception = self.__get_exception_from_init(*params)
-        self.assertIsNotNone(exception)
+    def test_drive_error_case(self):
+        with self.assertRaises(Exception) as ex:
+            self.car.drive(10)
+        self.assertEqual("You don't have enough fuel to drive!", str(ex.exception))
 
-    def test_carInit_WithZeroFuelCapacity_RaiseException(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 0
-        params = [make, model, fuel_consumption, fuel_capacity]
-        exception = self.__get_exception_from_init(*params)
-        self.assertIsNotNone(exception)
-
-    def test_carInit_WithNegativeFuelAmount_RaiseException(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        car = Car(*params)
-        with self.assertRaises(Exception) as context:
-            car.fuel_amount = -1
-        self.assertIsNotNone(context.exception)
-
-    def test_carRefuel_WithNegativeFuel_RaiseException(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        car = Car(*params)
-        with self.assertRaises(Exception) as context:
-            car.refuel(-1)
-        self.assertIsNotNone(context.exception)
-
-    def test_carRefuel_WithZeroFuel_RaiseException(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        car = Car(*params)
-        with self.assertRaises(Exception) as context:
-            car.refuel(0)
-        self.assertIsNotNone(context.exception)
-
-    def test_carRefuel_WithPositiveFuel_IncreaseAmount(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        car = Car(*params)
-        car.refuel(5)
-        self.assertEqual(5, car.fuel_amount)
-
-    def test_carRefuel_WithMoreFuel_SetAmount(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        car = Car(*params)
-        car.refuel(car.fuel_capacity * 2)
-        self.assertEqual(car.fuel_capacity, car.fuel_amount)
-
-    def test_carDrive_WithNotEnoughFuel_RaiseException(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        car = Car(*params)
-        car.refuel(car.fuel_capacity)
-        distance = 100
-        car.drive(distance)
-        expected = car.fuel_capacity - car.fuel_consumption * distance / 100
-        actual = car.fuel_amount
-        self.assertEqual(expected, actual)
-
-    def test_carDrive_WithEnoughFuel_DecreaseAmount(self):
-        make = "Test make"
-        model = "Test model"
-        fuel_consumption = 6
-        fuel_capacity = 60
-        params = [make, model, fuel_consumption, fuel_capacity]
-        car = Car(*params)
-        with self.assertRaises(Exception) as context:
-            car.drive(100)
-        self.assertIsNotNone(context.exception)
+    def test_drive_correct_case(self):
+        self.car.fuel_amount = 100
+        self.car.drive(1000)
+        self.assertEqual(44, self.car.fuel_amount)
 
 
 if __name__ == "__main__":
